f2bbe36b823bf48e84f8b6cd543a581c9ad908c2
mahdi amini <classicus.ma@gmail.com>
2025-04-05 20:46:51 +0330

    Added EnumExtensions in Common.

---
+++ Blog_Common/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Common
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = field?.GetCustomAttributes<DisplayAttribute>().FirstOrDefault();
+            return attribute?.Name ?? value.ToString();
+        }
+    }
+}
