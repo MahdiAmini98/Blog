394131ef821c5eb5626f04949ff0ed20c4de33cc
mahdi amini <classicus.ma@gmail.com>
2025-04-08 21:47:37 +0330

    Added Blog.PanelAdmin Project.

---
+++ Blog.PanelAdmin/Program.cs
@@ -0,0 +1,29 @@
+﻿using Blog.PanelAdmin;
+using Blog.PanelAdmin.Services.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+#region Authorize
+//1-add package webassymbly.Authorize
+
+//2-add component in main Layout
+
+//3- add services
+
+//سرویس اضافه شدن Authorization
+builder.Services.AddAuthorizationCore();
+
+// این اطلاعات کاربری که لاگین شده را به همه کامپوننت ها ارسال می کند
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+#endregion
+await builder.Build().RunAsync();
---
+++ Blog.PanelAdmin/Services/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Blog.PanelAdmin.Services.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"مهدی امینی "),
+                new Claim(ClaimTypes.Role,"Admin"),
+                new Claim(ClaimTypes.Email,"Admin@admin.com"),
+            };
+
+            var identity = new ClaimsIdentity(claims, "testAuthentication");
+            var user = new ClaimsPrincipal(identity);
+
+            return await Task.FromResult(new AuthenticationState(user));
+
+        }
+    }
+}
