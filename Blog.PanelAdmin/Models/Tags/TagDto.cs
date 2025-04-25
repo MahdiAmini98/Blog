649ae5d15568b92b049679952f0ca4199837167c
mahdi amini <classicus.ma@gmail.com>
2025-04-25 09:18:55 +0330

    Added Tag Service.

---
+++ Blog.PanelAdmin/Models/Tags/TagDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Blog.PanelAdmin.Models.Tags
+{
+    public class TagDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class CreateTagRequestDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class UpdateTagRequestDto
+    {
+        public string Name { get; set; } 
+    }
+}
---
+++ Blog.PanelAdmin/Services/Tags/ITagService.cs
@@ -0,0 +1,51 @@
+﻿using Blog.PanelAdmin.Models.Tags;
+using System.Net.Http.Json;
+
+namespace Blog.PanelAdmin.Services.Tag
+{
+    public interface ITagService
+    {
+        Task<List<TagDto>> GetTagsAsync();
+        Task<TagDto> GetTagByIdAsync(Guid id);
+        Task<TagDto> CreateTagAsync(CreateTagRequestDto request);
+        Task UpdateTagAsync(Guid id, UpdateTagRequestDto request);
+        Task DeleteTagAsync(Guid id);
+    }
+
+
+    public class TagService : ITagService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TagService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<TagDto> CreateTagAsync(CreateTagRequestDto request)
+        {
+            var response = await _httpClient.PostAsJsonAsync("/api/Tag", request);
+            return await response.Content.ReadFromJsonAsync<TagDto>()?? new();
+        }
+
+        public async Task DeleteTagAsync(Guid id)
+        {
+            await _httpClient.DeleteAsync($"api/Tag/{id}");
+        }
+
+        public async Task<TagDto> GetTagByIdAsync(Guid id)
+        {
+           return await _httpClient.GetFromJsonAsync<TagDto>($"api/Tag/{id}")??new();
+        }
+
+        public async Task<List<TagDto>> GetTagsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<TagDto>>("api/Tag") ?? new();
+        }
+
+        public async Task UpdateTagAsync(Guid id, UpdateTagRequestDto request)
+        {
+            await _httpClient.PutAsJsonAsync($"/api/Tag/{id}", request);
+        }
+    }
+}
